b3b1923b72e106f195d54d74ab7d85628c24443b
Fahmi Al <mfahmiak@gmail.com>
2025-12-28 21:41:39 +0700

    ci(tests): update CI workflow and package scripts for new test layout and vscode runner

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,14 @@
 import { defineConfig } from "@vscode/test-cli";
 
-export default defineConfig({
-  files: "out/test/**/*.test.js",
-});
+export default defineConfig([
+  {
+    label: "integration",
+    files: "out/tests/integration/suite/**/*.test.js",
+    version: "stable",
+    workspaceFolder: "examples",
+    mocha: {
+      ui: "tdd",
+      timeout: 20000,
+    },
+  },
+]);
