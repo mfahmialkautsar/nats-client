789a71247293551ff5ee5543805558e9002c2f5d
Fahmi Al <mfahmiak@gmail.com>
2026-02-08 11:05:16 +0700

    refactor(code-lens): simplify lens creation via factory methods

--- src/features/code-lens/nats-code-lens-provider.ts
+++ src/features/code-lens/nats-code-lens-provider.ts
@@ -2,6 +2,7 @@ import * as vscode from "vscode";
 import { parseNatsDocument } from "@/core/nats-document-parser";
 import type { NatsSession } from "@/services/nats-session";
 import type { VariableStore } from "@/services/variable-store";
+import type { NatsAction } from "@/core/nats-actions";
 
 const FILE_GLOB = "**/*.nats";
 
@@ -38,99 +39,126 @@ export class NatsCodeLensProvider implements vscode.CodeLensProvider {
         0,
       );
       const args: [string, number] = [document.fileName, action.lineNumber + 1];
-      switch (action.type) {
-        case "subscribe": {
-          const key = buildKey(document.fileName, action.lineNumber + 1);
-          const isSubscribed = this.session.isSubscribed(key);
-          const activeCount = this.session.getSubscriptionCount(action.subject);
-          codeLenses.push(
-            new vscode.CodeLens(range, {
-              title: `${isSubscribed ? "Unsubscribe" : "Subscribe"}${formatCount(activeCount)}`,
-              command: isSubscribed
-                ? "nats.stopSubscription"
-                : "nats.startSubscription",
-              arguments: args,
-            }),
-          );
-          break;
-        }
-        case "request":
-          codeLenses.push(
-            new vscode.CodeLens(range, {
-              title: "Send Request",
-              command: "nats.sendRequest",
-              arguments: args,
-            }),
-          );
-          break;
-        case "publish":
-          codeLenses.push(
-            new vscode.CodeLens(range, {
-              title: "Publish",
-              command: "nats.publish",
-              arguments: args,
-            }),
-          );
-          break;
-        case "reply": {
-          const key = buildKey(document.fileName, action.lineNumber + 1);
-          const active = this.session.isReplyHandlerActive(key);
-          const handlerCount = this.session.getReplyHandlerCount(
-            action.subject,
-          );
-          codeLenses.push(
-            new vscode.CodeLens(range, {
-              title: `${active ? "Stop Reply Handler" : "Start Reply Handler"}${formatCount(handlerCount)}`,
-              command: active
-                ? "nats.stopReplyHandler"
-                : "nats.startReplyHandler",
-              arguments: args,
-            }),
-          );
-          break;
-        }
-        case "jetstreamPublish":
-          codeLenses.push(
-            new vscode.CodeLens(range, {
-              title: "JetStream Publish",
-              command: "nats.jetstreamPublish",
-              arguments: args,
-            }),
-          );
-          break;
-        case "jetstreamConsume": {
-          const stream = action.stream
-            ? this.variableStore.resolveText(action.stream)
-            : "";
-          const durable = action.durable
-            ? this.variableStore.resolveText(action.durable)
-            : "";
-          const server = action.server
-            ? this.variableStore.resolveText(action.server)
-            : "";
-          const key = `${server}|${stream}/${durable}`;
-          const isSubscribed = this.session.isSubscribed(key);
-          const activeCount = this.session.getSubscriptionCount(
-            `${stream}/${durable}`,
-          );
-
-          codeLenses.push(
-            new vscode.CodeLens(range, {
-              title: `${isSubscribed ? "Stop Consumption" : "Start Consumption"}${formatCount(activeCount)}`,
-              command: isSubscribed
-                ? "nats.stopJetStreamConsume"
-                : "nats.startJetStreamConsume",
-              arguments: args,
-            }),
-          );
-          break;
-        }
-        default:
-          break;
+      const lens = this.createLens(document, action, range, args);
+      if (lens) {
+        codeLenses.push(lens);
       }
     }
 
     return codeLenses;
+  }
+
+  private createLens(
+    document: vscode.TextDocument,
+    action: NatsAction,
+    range: vscode.Range,
+    args: [string, number],
+  ): vscode.CodeLens | undefined {
+    switch (action.type) {
+      case "subscribe":
+        return this.createSubscribeLens(
+          document,
+          action as NatsAction & { type: "subscribe" },
+          range,
+          args,
+        );
+      case "request":
+        return new vscode.CodeLens(range, {
+          title: "Send Request",
+          command: "nats.sendRequest",
+          arguments: args,
+        });
+      case "publish":
+        return new vscode.CodeLens(range, {
+          title: "Publish",
+          command: "nats.publish",
+          arguments: args,
+        });
+      case "reply":
+        return this.createReplyLens(
+          document,
+          action as NatsAction & { type: "reply" },
+          range,
+          args,
+        );
+      case "jetstreamPublish":
+        return new vscode.CodeLens(range, {
+          title: "JetStream Publish",
+          command: "nats.jetstreamPublish",
+          arguments: args,
+        });
+      case "jetstreamConsume":
+        return this.createJetStreamConsumeLens(
+          action as NatsAction & { type: "jetstreamConsume" },
+          range,
+          args,
+        );
+      default:
+        return undefined;
+    }
+  }
+
+  private createSubscribeLens(
+    document: vscode.TextDocument,
+    action: NatsAction & { type: "subscribe" },
+    range: vscode.Range,
+    args: [string, number],
+  ): vscode.CodeLens {
+    const key = buildKey(document.fileName, action.lineNumber + 1);
+    const isSubscribed = this.session.isSubscribed(key);
+    const activeCount = this.session.getSubscriptionCount(action.subject);
+    return new vscode.CodeLens(range, {
+      title: `${isSubscribed ? "Unsubscribe" : "Subscribe"}${formatCount(activeCount)}`,
+      command: isSubscribed
+        ? "nats.stopSubscription"
+        : "nats.startSubscription",
+      arguments: args,
+    });
+  }
+
+  private createReplyLens(
+    document: vscode.TextDocument,
+    action: NatsAction & { type: "reply" },
+    range: vscode.Range,
+    args: [string, number],
+  ): vscode.CodeLens {
+    const key = buildKey(document.fileName, action.lineNumber + 1);
+    const active = this.session.isReplyHandlerActive(key);
+    const handlerCount = this.session.getReplyHandlerCount(action.subject);
+    return new vscode.CodeLens(range, {
+      title: `${active ? "Stop Reply Handler" : "Start Reply Handler"}${formatCount(handlerCount)}`,
+      command: active ? "nats.stopReplyHandler" : "nats.startReplyHandler",
+      arguments: args,
+    });
+  }
+
+  private createJetStreamConsumeLens(
+    action: NatsAction & { type: "jetstreamConsume" },
+    range: vscode.Range,
+    args: [string, number],
+  ): vscode.CodeLens {
+    const stream = action.stream
+      ? this.variableStore.resolveText(action.stream)
+      : "";
+    const durable = action.durable
+      ? this.variableStore.resolveText(action.durable)
+      : "";
+    const server = action.server
+      ? this.variableStore.resolveText(action.server)
+      : "";
+    const key = `${server}|${stream}/${durable}`;
+    const isSubscribed = this.session.isSubscribed(key);
+    const activeCount = this.session.getSubscriptionCount(
+      `${stream}/${durable}`,
+    );
+
+    return new vscode.CodeLens(range, {
+      title: `${isSubscribed ? "Stop Consumption" : "Start Consumption"}${formatCount(activeCount)}`,
+      command: isSubscribed
+        ? "nats.stopJetStreamConsume"
+        : "nats.startJetStreamConsume",
+      arguments: args,
+    });
   }
 }
 
